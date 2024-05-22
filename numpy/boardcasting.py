3f7d3a70824e715f68946e5b383cf4ba08826262
Somya Jha <sumicode.exe@gmail.com>
2024-05-22 23:01:46 +0530

    add

--- numpy/boardcasting.py
+++ numpy/boardcasting.py
@@ -1,4 +1,8 @@
+#numpy's meshgrid creates two-dimensional grids
 import numpy as np
 X, Y = np.meshgrid(np.arange(2), np.arange(2))
 print("array X:", X)
 print("array Y:", Y)
+
+#To add X and Y
+print("The sum is: ", X+Y)
