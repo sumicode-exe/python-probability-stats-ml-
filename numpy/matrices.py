0f3a6c7d4d06ab7cea5a5133a351438a50c5ba82
Somya Jha <sumicode.exe@gmail.com>
2024-05-22 22:56:14 +0530

    array mul

--- numpy/matrices.py
+++ numpy/matrices.py
@@ -1,7 +1,16 @@
+#multiplication of matrices
 import numpy as np
 A = np.matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-x = np.matrix([[1], [0], [1]])
+x = np.matrix([[1], [0], [0]])
 print("the first matrix is: ", A)
 print("the second matrix is: ", x)
 product = A * x
+print("The product of the matrices is:", product)
+
+#multiplication of arrays
+A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+x = np.array([[1], [0], [0]])
+print("the first matrix is: ", A)
+print("the second matrix is: ", x)
+product = A.dot(x)
 print("The product of the matrices is:", product)
