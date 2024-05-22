7c2ae5f9538041d48da9297add4b1eaa4d95b56d
Somya Jha <sumicode.exe@gmail.com>
2024-05-22 22:58:58 +0530

    matrix

--- numpy/arrays-and-matrices.py
+++ numpy/arrays-and-matrices.py
@@ -66,4 +66,26 @@ k = 5
 y = as_strided(x, (k, n-k+1), (x.itemsize, ) * 2)
 print("after explicit dimension manippulation is:", y)
 
+#multiplication of matrices
+import numpy as np
+A = np.matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+x = np.matrix([[1], [0], [0]])
+print("the first matrix is: ", A)
+print("the second matrix is: ", x)
+product = A * x
+print("The product of the matrices is:", product)
+
+#multiplication of arrays
+A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+x = np.array([[1], [0], [0]])
+print("the first matrix is: ", A)
+print("the second matrix is: ", x)
+product = A.dot(x)
+
+#Num-arrays suppoers element wise multiplication
+#It does not support row-column multiplication
+print("The product of the matrices is:", product)
+newSymforMul = A@x
+print("New symbol product is: ", newSymforMul)
+
 
