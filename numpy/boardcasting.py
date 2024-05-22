d5a1f1f311939cd3a7335eecdfd9ebdf5edbbb11
Somya Jha <sumicode.exe@gmail.com>
2024-05-22 23:06:26 +0530

    broadcast

--- numpy/boardcasting.py
+++ numpy/boardcasting.py
@@ -1,8 +1,19 @@
-#numpy's meshgrid creates two-dimensional grids
+# numpy's meshgrid creates two-dimensional grids
 import numpy as np
 X, Y = np.meshgrid(np.arange(2), np.arange(2))
 print("array X:", X)
 print("array Y:", Y)
 
-#To add X and Y
+# To add X and Y
 print("The sum is: ", X+Y)
+
+# We can skip a step, not bother with the meshgrid
+# We can implicitly obtain the vertex coordinates
+# by using broadcasting
+x = np.array([0, 1])
+y = np.array([0, 1])
+print("array x: ", x)
+print("array y: ", y)
+# this adds a broadcasting dimension
+sum = x+y[:, None]
+print("sum is: ", sum)
